19b67a28368a95967e8e485c405fb19427b59b5b
luis peña <penagarcia.luisenrique@gmail.com>
2023-05-16 12:55:45 -0600

    cambio de base de datos sqlite3 a railway

--- pos/settings.py
+++ pos/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LpPFtNT8QSRn6QxaITCj',
+        'HOST': 'containers-us-west-96.railway.app',
+        'PORT': '5485',
     }
 }
 
