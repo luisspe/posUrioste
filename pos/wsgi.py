5d0b936ee9cdfb18189cfb2cffcd85514bdf8128
luis peña <penagarcia.luisenrique@gmail.com>
2023-05-17 11:13:54 -0600

    dsf

--- pos/wsgi.py
+++ pos/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ems.settings')
 
-application = get_wsgi_application(set_prefix=False)
+application = get_wsgi_application()
