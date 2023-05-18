165021da4e8db36e343062e195444aab00f1cf03
luis peña <penagarcia.luisenrique@gmail.com>
2023-05-18 12:20:20 -0600

    got fdjkl

--- pos/settings.py
+++ pos/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_ROOT = 'https://posurioste-production.up.railway.app/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 DISABLE_COLLECTSTATIC = 0
 
 CSRF_TRUSTED_ORIGINS = [
