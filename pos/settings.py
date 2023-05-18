5a24cce5e69520a3abc62dde976036c591283a5c
luis peña <penagarcia.luisenrique@gmail.com>
2023-05-17 23:03:56 -0600

    cagandola xdd

--- pos/settings.py
+++ pos/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 DISABLE_COLLECTSTATIC = 0
 
