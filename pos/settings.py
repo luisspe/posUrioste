f957cd5cc6f987e5570f1c713f57a5969f0898de
luis peña <penagarcia.luisenrique@gmail.com>
2023-05-17 23:31:59 -0600

    sdf

--- pos/settings.py
+++ pos/settings.py
@@ -127,9 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = BASE_DIR / 'static'
-STATIC_ROOT = BASE_DIR / 'files'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DISABLE_COLLECTSTATIC = 0
 
