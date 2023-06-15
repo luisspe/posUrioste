1ef142c4322e7173db70b8caeb96202af22c6076
luis peña <penagarcia.luisenrique@gmail.com>
2023-06-15 16:09:12 -0600

    cambios largo comentario

--- posApp/models.py
+++ posApp/models.py
@@ -143,7 +143,7 @@ class Sales(models.Model):
     client = models.ForeignKey(Clientes, on_delete=models.CASCADE)
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     tipoPago = models.ForeignKey(FormaPago, on_delete=models.CASCADE) 
-    comentario = models.CharField(max_length=300, null=True)
+    comentario = models.CharField(max_length=700, null=True)
     def __str__(self):
         return self.code
 
