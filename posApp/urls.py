22d1497c91ded1366940c380af47f93ff8a2316e
Luis Peña <luispena@Luiss-MacBook-Pro.local>
2024-10-06 01:49:17 -0600

    creo que el error eran los url paths

--- posApp/urls.py
+++ posApp/urls.py
@@ -7,7 +7,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('redirect-admin', RedirectView.as_view(url="/admin"),name="redirect-admin"),
     path('', views.home, name="home-page"),
-    path('login', auth_views.LoginView.as_view(template_name = 'posApp/login.html',redirect_authenticated_user=True), name="login"),
+    path('login', views.CustomLoginView.as_view(), name="login"),
     path('userlogin', views.login_user, name="login-user"),
     path('logout', views.logoutuser, name="logout"),
     path('category', views.category, name="category-page"),
